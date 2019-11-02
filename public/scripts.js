d6fee949e08576e65deec770c3c6f7c06dcefebe
Grant Kim <grant.kim@duke.edu>
2019-11-02 07:40:16 -0400

    initial commit

---
+++ public/scripts.js
@@ -0,0 +1,13 @@
+
+
+const playSong = () => {
+    let song = document.getElementById("song"); 
+    song.play();
+}
+
+document.getElementById("song-player").addEventListener("click", playSong); 
+
+
+const beginGame = function() {
+    
+}
